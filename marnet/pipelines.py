71180efd2eac754a18f7c3d443dcb61c97fabe00
Damjan Georgievski <gdamjan@gmail.com>
2011-07-29 00:29:42 +0200

    Switch to couchdbkit

--- marnet/pipelines.py
+++ marnet/pipelines.py
@@ -2,13 +2,25 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
-import couchdb
+from couchdbkit import Database
+from scrapy.conf import settings
+
 
 class MarnetPipeline(object):
     """Store in CouchDB, check if it's new or changed item"""
     def __init__(self):
-        self.db = couchdb.Database("http://localhost:5984/dns")
+        self.db = Database(settings.get('COUCHDB_URL'))
 
     def process_item(self, domain, item):
-        self.db[item['domain']] = dict(item)
+        data = dict(item)
+        id = data.pop('domain')
+        old_data = self.db.get(id, None)
+        compare(old_data, data)
+        self.db[id] = data
         return item
+
+def compare(old_item, new_item):
+    old_item.pop('last_updated')
+    old_item.pop('_id')
+    old_item.pop('_rev')
+    new_item.pop('last_updated')
